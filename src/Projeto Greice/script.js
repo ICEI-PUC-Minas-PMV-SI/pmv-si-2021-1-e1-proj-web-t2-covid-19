7001e231b556b178cc3cf802226ac56f3904c7fc
romulogm <romulogm@gmail.com>
2021-06-15 18:48:47 -0300

    Testes

---
+++ src/Projeto Greice/script.js
@@ -0,0 +1,16 @@
+
+if (document.getElementById(proximo1).clicked == true){
+
+    localStorage.setItem("servico", document.getElementById("servico"));
+    localStorage.setItem("categoria", document.getElementById("categoria"));
+
+}
+
+
+document.getElementById("test").addEventListener('click',function ()
+{
+  
+   }  ); 
+
+
+document.getElementById(cadastroFinal).innerHTML =  
