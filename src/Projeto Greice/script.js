8b273462c8bd0da9cbc20256e8a4f9533ea2fe52
romulogm <romulogm@gmail.com>
2021-06-21 21:03:40 -0300

    Update JS

--- src/Projeto Greice/script.js
+++ src/Projeto Greice/script.js
@@ -2,11 +2,11 @@
 
 document.getElementById("proximo1").addEventListener("click", function(){ 
 
-    var varServico = document.getElementById("servico").value;
-    localStorage.setItem("dadoServico", varServico.value);
+    var varServico = document.getElementById('servico').value;
+    localStorage.setItem("dadoServico", varServico);
 
-    var varCategoria = document.getElementById("categoria").value;
-    localStorage.setItem("dadoCategoria", varCategoria.value);
+    var varCategoria = document.getElementById('categoria').value;
+    localStorage.setItem("dadoCategoria", varCategoria);
 
 }); 
 
@@ -14,51 +14,62 @@ document.getElementById("proximo1").addEventListener("click", function(){
 
 document.getElementById("proximo2").addEventListener("click", function(){ 
 
-    var local = localStorage.setItem("local", document.getElementById("local"));
-    var data = localStorage.setItem("data", document.getElementById("data"));
-    var horario = localStorage.setItem("horario", document.getElementById("horario"));
+    var varLocal = document.getElementById("local").value;
+    localStorage.setItem("dadoLocal", varLocal);
+    
+    var varData = document.getElementById("data").value;
+    localStorage.setItem("dadoData", varData);    
+
+    var varHorario = document.getElementById("horario").value;
+    localStorage.setItem("dadoHorario", varHorario);    
 }); 
 
 /*  Página Nome, Sexo, Nascimento, CPF, Telefone, E-mail */
 
 document.getElementById("proximo3").addEventListener("click", function(){ 
 
-    var nome = localStorage.setItem("local", document.getElementById("local"));
-    var sexo = localStorage.setItem("sexo", document.getElementById("sexo"));
-    var nascimento = localStorage.setItem("nascimento", document.getElementById("nascimento"));
-    var cpf = localStorage.setItem("cpf", document.getElementById("cpf"));
-    var telefone = localStorage.setItem("telefone", document.getElementById("telefone"));
-    var email = localStorage.setItem("email", document.getElementById("email"));
+  
+    var varNome = document.getElementById("nome").value;
+    localStorage.setItem("dadoNome", varNome);
+
+    var varSexo = document.getElementById("sexo").value;
+    localStorage.setItem("dadoSexo", varSexo);
+
+    var varNascimento = document.getElementById("nascimento").value;
+    localStorage.setItem("dadoNascimento", varNascimento);
+
+    var varCPF = document.getElementById("cpf").value;
+    localStorage.setItem("dadoCPF", varCPF);
+
+    var varTelefone = document.getElementById("telefone").value;
+    localStorage.setItem("dadoTelefone", varTelefone);
+
+    var varEmail = document.getElementById("email").value;
+    localStorage.setItem("dadoEmail", varEmail);
 }); 
 
 
-/* Página Final */
+/* Print Cadastro - Conclusão e consulta de cadastro*/
 
 document.getElementById("cadastroFinal").addEventListener("onload", function ()
 {
     document.getElementById("cadastroFinal").innerHTML = {
-
-        servico 
-        categoria
-        local
-        data
-        horario
-        nome
-        sexo
-        nascimento
-        cpf
-        telefone
-        email
-
+        
+        localStorage.getItem("dadoServico");
+        localStorage.getItem("dadoCategoria");
+        localStorage.getItem("dadoLocal");
+        localStorage.getItem("dadoData");
+        localStorage.getItem("dadoHorario");
+        localStorage.getItem("dadoNome");
+        localStorage.getItem("dadoSexo");
+        localStorage.getItem("dadoNascimento");
+        localStorage.getItem("dadoCPF");
+        localStorage.getItem("dadoTelefone");
+        localStorage.getItem("dadoEmail");
 
     }
    }  ); 
 
-
- /* Página Consulta */
-
-
-  
 
 
 
